1488c3065d8a66e7d9f95704b970018bbd477c0a
Yernemm <yrnemmizer@gmail.com>
2022-07-31 23:55:46 +0100

    start

---
+++ classes/FolderManager.ts
@@ -0,0 +1,15 @@
+class FolderManager{
+
+    dir : string;
+    recursive : boolean;
+
+    constructor(dir : string, recursive : boolean){
+
+        this.dir = dir;
+        this.recursive = recursive;
+
+    }
+
+}
+
+this.exports = FolderManager;
