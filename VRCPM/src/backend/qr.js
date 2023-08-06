65915a7d054df66bbd5456f2a9674c35c51bf5b5
Yernemm <yrnemmizer@gmail.com>
2023-08-06 06:53:13 +0100

    this seems to work worse than the other appraoch

--- VRCPM/src/backend/qr.js
+++ VRCPM/src/backend/qr.js
@@ -1,39 +1,29 @@
-const qrCodeReader = require('qrcode-reader');
-        // __ Importing jimp __ \\
+const jsQR = require('jsqr');
 const Jimp = require("jimp");
+const fs = require('fs')
+const { get, getSync } = require('@andreekeberg/imagedata')
+
 const scan = (image, callback) => {
 
 
-    // __ Importing filesystem = __ \\
-    const fs = require('fs')
+    get(image, (error, data) =>{
+        if(!error){
 
-    // __ Importing qrcode-reader __ \\
+        
+        const code = jsQR(data.data, data.width, data.height);
+
+        console.log(data);
+        console.log(code)
     
-    
-    // __ Read the image and create a buffer __ \\
-    const buffer = fs.readFileSync(image);
-    
-    // __ Parse the image using Jimp.read() __ \\
-    Jimp.read(buffer, function(err, image) {
-        if (err) {
-            console.error(err);
+        callback(code ? code.data : null)
+        }else{
+            callback(null)
         }
-    // __ Creating an instance of qrcode-reader __ \\
+    })
 
-        const qrCodeInstance = new qrCodeReader();
 
-        qrCodeInstance.callback = function(err, value) {
-            if (err) {
-                console.error(err);
-            }
-    // __ Printing the decrypted value __ \\
-            //console.log(value.result);
-            callback(err ? null : value.result);
-        };
 
-    // __ Decoding the QR code __ \\
-        qrCodeInstance.decode(image.bitmap);
-    });
 }     
+
 
 module.exports = {scan};
